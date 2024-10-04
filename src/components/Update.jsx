3703e01de557e531e6cce1a563337b8d0d4c7637
rezaulislam54 <rezaulislam5490@gmail.com>
2024-10-04 22:48:08 +0600

    user update section create

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,14 +1,63 @@
 import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
+  const updatedUser = useLoaderData();
+  // console.log(updatedUser);
 
-    const updatedUser = useLoaderData();
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const remaininguser = { name, email };
+    console.log(remaininguser);
 
-    return (
-        <div>
-            <h1>Name : {updatedUser.name}</h1>
-        </div>
-    );
+    fetch(`http://localhost:5000/users/${updatedUser._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(remaininguser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          alert("User Updated Successfully!");
+          form.reset();
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h1>Update Your Information</h1>
+      <div className="shadow-lg border rounded-md p-10 mt-10 w-fit mx-auto">
+        <form onSubmit={handleUpdate}>
+          <input
+            className="border bg-gray-100 mb-3 rounded-md px-4"
+            type="text"
+            defaultValue={updatedUser.name}
+            name="name"
+            id=""
+          />
+          <br />
+          <input
+            className="border bg-gray-100 rounded-md px-4"
+            type="email"
+            name="email"
+            id=""
+            defaultValue={updatedUser.email}
+          />
+          <br />
+          <input
+            type="submit"
+            value="Update"
+            className="bg-yellow-500 text-white rounded-lg px-4 mt-4"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Update;
